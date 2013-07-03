00488fdeee3eaa4f137e65679b09bbacab039fc0
PeterCxy <PeterCxy@outlook.com>
2013-07-03 13:35:02 +0800

    update 3.4 kernel for cm-10.1
    Change-Id: I71ab8f1455522bb6cfaef9d5c78c9ef27213e063

--- mokee.mk
+++ mokee.mk
@@ -1,5 +1,5 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/mk/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := mione_plus
@@ -7,7 +7,7 @@ PRODUCT_RELEASE_NAME := mione_plus
 TARGET_BOOTANIMATION_NAME := vertical-480x854
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common.mk)
+$(call inherit-product, vendor/mk/config/common.mk)
 
 # old audio files
 include frameworks/base/data/sounds/OldAudio.mk
@@ -35,7 +35,7 @@ $(call inherit-product, device/xiaomi/mione_plus/device_mione_plus.mk)
 
 # Setup device specific product configuration.
 PRODUCT_DEVICE := mione_plus
-PRODUCT_NAME := cm_mione_plus
+PRODUCT_NAME := mk_mione_plus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI-ONE Plus
 PRODUCT_MANUFACTURER := Xiaomi
@@ -49,4 +49,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 	BUILD_FINGERPRINT="Xiaomi/mione_plus/mione_plus:4.1.2/JZO54K/QDS84:userdebug/test-keys" \
 	PRIVATE_BUILD_DESC="mione_plus-userdebug 4.1.2 JZO54K QDS84 test-keys" \
 	BUILD_NUMBER=JDQ39E
-
