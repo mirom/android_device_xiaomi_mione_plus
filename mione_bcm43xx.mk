5f8ddeb1922bb26a9099e0f6b3ce5b36fb4394c4
PeterCxy <PeterCxy@outlook.com>
2013-07-08 13:18:02 +0800

    Update configs and kernel modules
    Change-Id: I1009b4fa236f358042bb80a40fb516bca7a39484

--- mione_bcm43xx.mk
+++ mione_bcm43xx.mk
@@ -6,16 +6,19 @@
 
 
 # common bcm43xx
-BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+BOARD_LEGACY_NL80211_STA_EVENTS  := true
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE                := bcmdhd
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_WLAN_DEVICE := bcmdhd
 WIFI_BAND := 802_11_ABG
 WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # old mione device have bcm4329 chip (M1, M1CU, M1C1, M1youth)
-WIFI_DRIVER_MODULE_NAME := bcm4329
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_MODULE_NAME := bcmdhd
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcmdhd.ko"
 WIFI_DRIVER_MODULE_ARG  := "firmware_path=/system/etc/firmware/fw_bcm4329.bin nvram_path=/system/etc/firmware/nvram.txt"
 WIFI_DRIVER_FW_PATH_STA := "/system/etc/firmware/fw_bcm4329.bin"
 WIFI_DRIVER_FW_PATH_AP  := "/system/etc/firmware/fw_bcm4329_apsta.bin"
